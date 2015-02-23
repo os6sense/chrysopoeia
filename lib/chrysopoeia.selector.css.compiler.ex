f3cedb232ba6a7527feb3aaf58e411be51c00598
os6sense <leej@sowhatresearch.com>
2015-02-23 19:22:10 +0000

    Moved anonymous functions into macros

--- lib/chrysopoeia.selector.css.compiler.ex
+++ lib/chrysopoeia.selector.css.compiler.ex
@@ -1,12 +1,35 @@
+# TODO: Compiler has to add spacee (i.e. decendant operator)
 
+# E F                     an F element descendant of an E element
+# E > F                   an F element child of an E element
+# E + F                   an F element immediately preceded by an E element
+# E ~ F                   an F element preceded by an E element
 
 defmodule Chrysopoeia.Selector.CSS.Compiler do
 
+  # Takes the output of the CSS parser and compiles the map into functions for matching
   defp compile(map) when is_list(map) do
     map
-      |> Enum.map( &(compile_selector(&1)) )
+      #|> Enum.map( &(compile_selector(&1)) )
+      |> Enum.map_reduce(nil, fn 
+                                (" ", nil)  -> { nil, " "}
+                                ("<", nil)  -> { nil, "<"}
+                                ("+", nil)  -> { nil, "+"}
+                                ("~", nil)  -> { nil, "~"}
+                                (e, " ")    -> { compile_selector(e, " "), nil}
+                                (e, "+")    -> { compile_selector(e, "+"), nil}
+                                (e, "~")    -> { compile_selector(e, "~"), nil}
+                                (e, nil)    -> { compile_selector(e), nil}
+      end)
   end
 
+  # E F                     an F element descendant of an E element
+  # return true if there is an e element in the path - does my path now include attributes?
+  def compile_selector(%{"type" => type, "attr" => "", "op" => "", 
+                         "value" => "", "ptype" => "", "pval" => "" }, " ") do
+    fn (e, a, meta) -> e == type end
+  end
+ 
   # takes a string which should be either a combinator function or a simple
   # selector
 
@@ -22,96 +45,79 @@ defmodule Chrysopoeia.Selector.CSS.Compiler do
     :combine
   end
 
-  def is_not_pseudo(m) do
-  end
-
-
-  # What about *?
-
-
-  # :simple_element
-  def compile_selector(%{"type" => type, "attr" => "", "op" => "", "value" => "", "ptype" => "", "pval" => "" }) do
-    fn (e, a, _meta) -> e == type end
-  end
-
-  # :attribute_only
-  def compile_selector(%{"type" => "", "attr" => attr, "op" => "", 
-                          "value" => "", "ptype" => "", "pval" => "" }) do
-    fn (e, a, _meta) -> Enum.any?(a, fn ({lattr, lval}) -> lattr == attr end) end
-  end
-
   defp test_with_op("=" , lval, value), do: lval == value
   defp test_with_op("^=", lval, value), do: String.starts_with?(lval, value)
   defp test_with_op("$=", lval, value), do: String.ends_with?(lval, value)
   defp test_with_op("*=", lval, value), do: String.contains?(lval, value)
   defp test_with_op("|=", lval, value), do: String.starts_with?(lval, value <> "|")
 
+  defmacro element_matches_type?(e, type) do
+    quote do
+      unquote(e) == unquote(type)
+    end
+  end
+
+  # :simple_element
+  def compile_selector(%{"type" => type, "attr" => "", "op" => "", 
+                         "value" => "", "ptype" => "", "pval" => "" }) do
+    fn (e, a, _meta) -> element_matches_type?(e, type) end# e == type end
+  end
+
+  defmacro has_attribute?(attributes, attribute_name) do
+    quote do
+      Enum.any?(unquote(attributes), fn 
+        ({lattr, lval}) -> lattr == unquote(attribute_name) 
+      end)
+    end
+  end
+
+  # :attribute_only
+  def compile_selector(%{"type" => "", "attr" => attr, "op" => "", 
+                          "value" => "", "ptype" => "", "pval" => "" }) do
+    #fn (e, a, _meta) -> Enum.any?(a, fn ({lattr, lval}) -> lattr == attr end) end
+    fn (e, a, _meta) -> has_attribute?(a, attr) end
+  end
+
+  defmacro has_attribute_with_value?(attributes, attribute_name, op, value) do
+    quote do
+      Enum.any?(unquote(attributes), fn 
+        ({lattr, lval}) -> lattr == unquote(attribute_name) 
+                           && test_with_op(unquote(op), lval, unquote(value))
+      end) 
+    end
+  end
+
   # :attribute_value
   def compile_selector(%{"type" => "", "attr" => attr, "op" => op, "value" => value, 
                           "ptype" => "", "pval" => "" }) do
-    #:attribute_value
-    fn (e, a, _meta) -> Enum.any?(a, fn ({lattr, lval}) -> 
-                    lattr == attr 
-                    && test_with_op(op, lval, value)
-                    #&& cond do
-                      #op == "=" -> lval == value
-                      #op == "^=" -> String.starts_with?(lval, value)
-                      #op == "$=" -> String.ends_with?(lval, value)
-                      #op == "*=" -> String.contains?(lval, value)
-                      #op == "|=" -> String.starts_with?(lval, value <> "|")
-                    #end
-      end) 
-    end
+    #fn (e, a, _meta) -> Enum.any?(a, fn ({lattr, lval}) -> 
+                    #lattr == attr && test_with_op(op, lval, value)
+      #end) 
+    fn (e, a, _meta) -> has_attribute_with_value?(a, attr, op, value) end
   end
 
   # :simple_element_attribute
   def compile_selector(%{"type" => type, "attr" => attr, "op" => "", "value" => "", 
                           "ptype" => "", "pval" => "" }) do
-    fn (e, a, _meta) -> e == type && Enum.any?(a, fn ({lattr, lval}) -> lattr == attr end) end
+    #fn (e, a, _meta) -> e == type && Enum.any?(a, fn ({lattr, lval}) -> lattr == attr end) end
+    fn (e, a, _meta) -> element_matches_type?(e, type) && has_attribute?(a, attr) end
   end
 
   # :simple_element_attribute_value
   def compile_selector(%{"type" => type, "attr" => attr, "op" => op, "value" => value, 
                           "ptype" => "", "pval" => "" }) do
-    fn (e, a, _meta) -> e == type 
-      && Enum.any?(a, fn ({lattr, lval}) -> lattr == attr 
-                        #&& lval == value end) 
-                        && test_with_op(op, lval, value) end)
-    end
+    #fn (e, a, _meta) -> e == type 
+      #&& Enum.any?(a, fn ({lattr, lval}) -> lattr == attr 
+                        #&& test_with_op(op, lval, value) end)
+    #end
+
+    fn (e, a, _meta) -> element_matches_type?(e, type) 
+                        && has_attribute_with_value?(a, attr, op, value) end
   end
 
   def compile_selector(%{"type" => type, "attr" => attr, "op" => op, 
                           "value" => value, "ptype" => ptype, "pval" => pval }) do
-    # fail, not implemented  
+    # fail, not yet implemented  
   end
-
-
-
-
-
-
-  # need to think about the match function in order to work this out
-  # mfns are the match functions
-  def match(e, a, meta, mfns) do
-    
-    # so say I have the above data to work with, I'm the b element within a p
-    # so take the simplist case
-    #List.last(mfns).(e, a, meta) # call the last match function 
-                                 # which is, e == "b"
-                                 # its true so we'd return true
-
-    # So harder, lets say we have "span p b" as our selector
-    #List.last(mfns).(e, a, meta) # call the last match function 
-                                 # which is, e == "b"
-                                 # its true so we do the next one using the
-                                 # next value in the path
-    #List.last(mfns, -1).(List.last(meta[:path], -1), those_args, meta)
-                                 # which is e == p
-
-    # So our starting point is that the e, a arguments passed into the compiled
-    # selector function may contain either the values of the current node
-    # OR those of the path
-  end
-
   
 end
